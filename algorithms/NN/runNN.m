fb7a7d3425b92faea86416ae23d85201b59b4781
lianguasth <lianchengliang@liancheiangsmbp.wv.cc.cmu.edu>
2014-11-02 16:20:25 -0500

    NN init version

--- algorithms/NN/runNN.m
+++ algorithms/NN/runNN.m
@@ -1,8 +1,10 @@
 clear
 clc
 %read in data and random shuffle to get the test data
-inputY = csvread('dotaLabel.csv');
-inputX = csvread('dotaFeature.csv');
+%inputY = csvread('dotaLabel.csv');
+%inputX = csvread('dotaFeature.csv');
+inputY = csvread('lolLabel.csv');
+inputX = csvread('lolFeature.csv');
 % useful info
 numData = size(inputX, 1);
 featDim = size(inputX, 2);
@@ -49,10 +51,12 @@ TrainY = YTrain(1:numTrainData,:);
     %  Backprop weight/bias learning function
     %  Performance function
 trainData = TrainX';
-targetData = TrainY';
+targetData = [1-TrainY TrainY]';
+
 PR = ones(108, 2);
 PR(:, 1) = -1;
-net = newff(PR, [72,72,1], {'tansig' 'tansig' 'tansig'},'trainlm','learngdm','mse');
+%output layer should use softmax transfer function
+net = newff(PR, [72,72,2], {'logsig' 'logsig' 'softmax'},'traingd','learngd','mse');
 net = configure(net,trainData,targetData);
 net = init(net);
 %%%define the weight of data test and validation
@@ -60,27 +64,34 @@ net = init(net);
 %net.divideParam.valRatio = .2;
 %net.divideParam.testRatio = .2;
 %%%set the training parameters
-net.trainParam.epochs =1000;	%(Max no. of epochs to train) [100]
-net.trainParam.goal =0.01;		%(stop training if the error goal hit) [0]
-net.trainParam.lr =0.001;		%(learning rate, not default trainlm) [0.01]
+net.trainParam.epochs =100;	%(Max no. of epochs to train) [100]
+net.trainParam.goal =0.001;		%(stop training if the error goal hit) [0]
+net.trainParam.lr =0.01;		%(learning rate, not default trainlm) [0.01]
 net.trainParam.show =1;		%(no. epochs between showing error) [25]
 net.trainParam.time =1000;	%(Max time to train in sec) [inf]
+
 
 %%
 %set the number of 
 net.trainParam.showWindow = 1;
 rmse = [];
 t = [];
+
+net = init(net);               % initialize network weights
 for i=1:nIter       %
-    net = init(net);               % initialize network weights
 
     tic
     net = train(net,trainData,targetData);       % train
-    predicted = sim(net, testX');      % test
+    predicted = sim(net, TestX');      % test
     t(i) = toc;
 
-    r = (testY' - predicted);         % residuals
-    rmse(i) = sqrt(mean(r.^2));    % root mean square error
+    r = ([1-TestY TestY]' - predicted);         % residuals
+    res = predicted(1, :) > predicted(2,:);
+    res = [1 - TestY]' - res;
+    ratio = 1.0*length(find(res == 0))/length(res);
+    display(ratio)
+    rmse(i) = sqrt(mean(r(1,:).^2 + r(2,:).^2));    % root mean square error
+    net.trainParam.lr = net.trainParam.lr*0.9;
 end
 
 % plot errors and elapsed times
